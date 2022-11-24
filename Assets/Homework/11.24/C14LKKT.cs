64a86f1e41731baa99f423ef45231963baf81677
LaszloPPP <laszlo.k.papp@gmail.com>
2022-11-24 11:45:54 +0100

    LKKT done

--- Assets/Homework/11.24/C14LKKT.cs
+++ Assets/Homework/11.24/C14LKKT.cs
@@ -3,47 +3,74 @@ using UnityEngine;
 
 public class C14LKKT : MonoBehaviour
 {
-    [SerializeField] int numA, numB, lkkt;
+    [SerializeField] int numA, numB, lkkt, lnko;
 
     private void OnValidate()
     {
-        lkkt = Lnko(numA, numB);
+
+        lnko = Lnko(numA, numB);
+        lkkt = Lkkt(numA, numB);
+        
     }
-    /*void Lkkt(int a, int b)
+    int Lkkt(int a, int b)
     {
-    int lkkt;
+        
+        int lkkt = (a * b) / Lnko(a, b);
+        return lkkt;
+        
     }
-    */
+    
 
     int Lnko(int a, int b)
+
+    /*
+     * Elegánsabban fogalmazva a módszer a következõ: elosztjuk a-t b-vel (a nagyobb számot a kisebbel - ha a két szám egyenlõ, akkor ln. k. o.-juk a=b), majd az osztási maradékkal b-t, és így tovább, akkor az utolsó nem nulla maradék maga az lnko lesz.[2]
+
+Példa:
+
+lnko(84, 18) = ?
+
+Ekkor elosztjuk 84-et 18-cal
+a hányados 4, a maradék 12
+elosztjuk 18-at 12-vel
+a hányados 1, a maradék 6
+elosztjuk 12-t 6-tal
+a hányados 2, a maradék 0,
+     */
     {
         if (a > b)
         {
+
             int remain = 0;
-            while (a / b > 0)
+            while (a % b > 0)
             {
                 int mod = a % b;
                 remain = mod;
+                a = b;
+                b = mod;
 
             }
             return remain;
         }
-        else if (b < a)
+        else if (b > a)
         {
+
             int remain = 0;
-            while (b / a > 0)
+            while (b % a > 0)
             {
-                int mod = a % b;
+                int mod = b % a;
                 remain = mod;
+                b = a;
+                a = mod;
 
             }
             return remain;
         }
-        else
-        {
-            return a;
-        }
+         return a;
+        
+
     }
+    
 }
 
 
